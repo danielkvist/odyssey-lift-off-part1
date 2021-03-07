27a4dba1598d535fcf56415007cd93cbe444dace
danielkvist <daniel@dkvist.com>
2021-03-07 15:06:07 +0100

    add apollo provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
+// The ApolloClient represents the Apollo client itself. Every
+// instance of ApolloClient uses an in-memory cache. This enables
+// it to store and reuse query results so it doesn't have to make
+// as many network requests.
+// The ApolloProvider uses React's Context API to make
+// a configured Apollo client instance available throughout
+// a React component tree.
+
+const client = new ApolloClient({
+	uri: 'http://localhost:4000',
+	cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ApolloProvider client={client}>
+			<GlobalStyles />
+			<Pages />
+		</ApolloProvider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
