642729aa4c30f64dc8c719d0f9349950cb696fae
danielkvist <daniel@dkvist.com>
2021-03-07 15:25:04 +0100

    display data

--- client/src/index.js
+++ client/src/index.js
@@ -13,16 +13,14 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 // a React component tree.
 
 const client = new ApolloClient({
-	uri: 'http://localhost:4000',
+	uri: 'http://localhost:4000/',
 	cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
-	<React.StrictMode>
-		<ApolloProvider client={client}>
-			<GlobalStyles />
-			<Pages />
-		</ApolloProvider>
-	</React.StrictMode>,
+	<ApolloProvider client={client}>
+		<GlobalStyles />
+		<Pages />
+	</ApolloProvider>,
 	document.getElementById('root')
 );
--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,43 @@
 import React from 'react';
 import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
+// By convention, query constants are in ALL_CAPS
+export const TRACKS = gql`
+	query Query {
+		tracksForHome {
+			id
+			title
+			thumbnail
+			length
+			modulesCount
+			author {
+				id
+				name
+				photo
+			}
+		}
+	}
+`;
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+	const { loading, error, data } = useQuery(TRACKS);
+
+	return (
+		<Layout grid>
+			<QueryResult error={error} loading={loading} data={data}>
+				{data?.tracksForHome?.map((track) => (
+					<TrackCard key={track.id} track={track}></TrackCard>
+				))}
+			</QueryResult>
+		</Layout>
+	);
 };
 
 export default Tracks;
